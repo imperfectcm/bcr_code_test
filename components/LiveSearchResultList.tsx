8119a999dbb8ab1248ec6eceff27da3f4c3fecc3
unknown <imperfect.cm@gmail.com>
2024-09-19 19:21:04 +0800

    finally add onClick function at default result

--- components/LiveSearchResultList.tsx
+++ components/LiveSearchResultList.tsx
@@ -1,6 +1,4 @@
 import { useRouter } from "next/navigation";
-import { createContext } from "react";
-
 
 interface LiveSearchResultListProps {
     liveSearchResult: any;
@@ -20,33 +18,33 @@ const LiveSearchResultList = (props: LiveSearchResultListProps) => {
     if (props.liveSearchResult && props.liveSearchResult.length > 0) {
 
         let displayResults = props.liveSearchResult.map((result: any, index: number) => {
-            console.log("result", result);
+
             return (
-                    <div key={index} onClick={(e) => handleOnClick(result, e)}
-                        className="w-full grid grid-rows-2 grid-cols-3 sm:grid-cols-5 gap-1 border border-slate-500 px-1 py-3 cursor-pointer">
-                        <div className="row-start-1 col-start-1 sm:col-start-2 col-span-3 flex justify-between">
-                            <span>
-                                <div className="text-sm font-bold">Competition:</div>
-                                <div className="text-sm">{result.competition_name}</div>
-                            </span>
-                            <span className="flex flex-col items-end">
-                                <div className="text-sm font-bold">Start Time:</div>
-                                <div className="text-sm">{result.fixture_datetime.replace(":00.000", "")}</div>
-                            </span>
-                        </div>
-
-                        <div className="row-start-2 col-start-1 sm:col-start-2 col-span-3 flex justify-between">
-                            <span>
-                                <div className="text-sm font-bold">Home Team:</div>
-                                <div className="text-sm">{result.home_team}</div>
-                            </span>
-                            <span className="flex flex-col items-end">
-                                <div className="text-sm font-bold">Away Team:</div>
-                                <div className="text-sm">{result.away_team}</div>
-                            </span>
-                        </div>
-
+                <div key={index} onClick={(e) => handleOnClick(result, e)}
+                    className="w-full grid grid-rows-2 grid-cols-3 sm:grid-cols-5 gap-1 border border-slate-500 px-1 py-3 cursor-pointer">
+                    <div className="row-start-1 col-start-1 sm:col-start-2 col-span-3 flex justify-between">
+                        <span>
+                            <div className="text-sm font-bold">Competition:</div>
+                            <div className="text-sm">{result.competition_name}</div>
+                        </span>
+                        <span className="flex flex-col items-end">
+                            <div className="text-sm font-bold">Start Time:</div>
+                            <div className="text-sm">{result.fixture_datetime.replace(":00.000", "")}</div>
+                        </span>
                     </div>
+
+                    <div className="row-start-2 col-start-1 sm:col-start-2 col-span-3 flex justify-between">
+                        <span>
+                            <div className="text-sm font-bold">Home Team:</div>
+                            <div className="text-sm">{result.home_team}</div>
+                        </span>
+                        <span className="flex flex-col items-end">
+                            <div className="text-sm font-bold">Away Team:</div>
+                            <div className="text-sm">{result.away_team}</div>
+                        </span>
+                    </div>
+
+                </div>
             )
         })
 
--- components/ResultDataContainer.tsx
+++ components/ResultDataContainer.tsx
@@ -1,17 +1,30 @@
-
+import { useRouter } from "next/navigation";
 
 interface ResultDataContainerProps {
     resultData: any;
 }
 
+
 const ResultDataContainer = (props: ResultDataContainerProps) => {
+
+
+    const router = useRouter();
+
+    const handleOnClick = async (result: any, e: React.MouseEvent<HTMLDivElement>) => {
+        e.preventDefault();
+        localStorage.removeItem("detail");
+        localStorage.setItem("detail", JSON.stringify(result));
+        router.push(`/match/${result._id}`);
+    }
+
 
     let displayData = props.resultData.map((result: any, index: number) => {
         return (
             <div key={index} className="col-span-1 flex-1 flex flex-col place-items-center 
                 scale-95 hover:scale-100 duration-200
                 bg-sky-900 text-neutral-100
-                rounded-lg py-5">
+                rounded-lg py-5 cursor-pointer"
+                onClick={(e) => handleOnClick(result, e)}>
 
                 <div className="text-base sm:text-lg font-bold text-sky-400">Competition:</div>
                 <div className="text-sm mb-2 sm:text-base sm:mb-0 sm:mb-0">{result.competition_name}</div>
